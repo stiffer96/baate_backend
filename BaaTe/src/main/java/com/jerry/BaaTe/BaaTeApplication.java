a550981723cfa69b0fe120fe2137b0d456d341b7
Sourav Choudhary <39512350+stiffer96@users.noreply.github.com>
2021-07-31 14:57:33 +0530

    Uploaded spring boot project
    added spring boot project with starter dependencies

---
+++ BaaTe/src/main/java/com/jerry/BaaTe/BaaTeApplication.java
@@ -0,0 +1,13 @@
+package com.jerry.BaaTe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaaTeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaaTeApplication.class, args);
+	}
+
+}
---
+++ BaaTe/src/test/java/com/jerry/BaaTe/BaaTeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerry.BaaTe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaaTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
